0667842feb1f44bb8818f8559b7c3a7c5831ba4e
Jonas Zaddach <jonas.zaddach@gmail.com>
2016-02-14 03:20:04 +0100

    Added python test

---
+++ tests/python_arm_nop/test.py
@@ -0,0 +1,14 @@
+import libqemu
+
+def memhandler(self, env, addr, size, signed, code):
+    #self.raise_error(env, 1)
+    return 0xe1a00000
+
+if __name__ == "__main__":
+    codeflags = libqemu.ArmCodeFlags(thumb = False)
+    lq = libqemu.Libqemu(memhandler, "/home/ida/projects/build/libqemu-test/libqemu/arm-lib/libqemu-arm.so")
+    code = lq.gen_intermediate_code(0, codeflags, True)
+    print(code)
+
+
+

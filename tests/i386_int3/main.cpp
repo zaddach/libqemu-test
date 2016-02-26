8d5c33a04a0cb3615c6aec48013f0ca5ef620c73
Jonas Zaddach <jonas.zaddach@gmail.com>
2016-02-26 09:47:44 +0100

    Added testcase for i386 int3 instruction

---
+++ tests/i386_int3/main.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <libqemu/qemu-lib-external.h>
+#include <llvm/Support/raw_ostream.h>
+
+uint64_t qemu_ld(void *env, uint64_t ptr, unsigned width, bool is_signed, bool is_code)
+{
+    return 0xcc;
+}
+
+int main(int argc, char *argv[])
+{
+    CodeFlags code_flags = {};
+    int error;
+    LLVMValueRef func;
+
+    libqemu_init(qemu_ld, NULL);
+
+    if ((error = libqemu_gen_intermediate_code(0, code_flags, true, &func)) == 0) {
+        char *func_str = LLVMPrintValueToString(func);
+        llvm::outs() << "SUCCESS: Translated instruction" << '\n';
+        llvm::outs() << func_str << '\n';
+        LLVMDisposeMessage(func_str);
+    }
+    else {
+        llvm::outs() << "ERROR: Got error code " << error << " while translating instruction" << '\n';
+        return 1;
+    }
+}
